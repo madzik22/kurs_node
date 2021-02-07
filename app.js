0d8fc2a85de275a9b68f91aef61007397e12210e
madzik22 <magda.kruczek@gmail.com>
2021-02-07 10:45:54 +0100

    add page title

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-console.log("Hello");
+const express = require("express");
+const path = require("path");
 
-const helloWorld =() => {
-    console.log("Hello World")
-}
+const port = 3000;
+const app = express();
 
-helloWorld();
+app.set("view engine","hbs");
+
+app.use("/assets",express.static(path.join(__dirname,"./assets")))
+app.use("/js",express.static(path.join(__dirname,"./js")))
+
+app.get("/", function (req,res) {
+    res.render('index',{
+        pageTitle: "dzisiejsza lekcja"
+    })
+})
+app.get("/about", function (req,res) {
+    res.send("<h2>Strona o nas</h2>")
+})
+
+app.listen(port, (err) => {
+    if (err) { return console.log(err)}
+    console.log("Działa na porcie",port)
+})
---
+++ functions.js
@@ -0,0 +1,12 @@
+const helloWorld =() => {
+    console.log("Hello World")
+}
+
+const add = (a,b) => { console.log(a+b)}
+
+module.exports = {
+    helloWorld,
+    add
+}
+
+console.log("Plik załadowano pomyślnie")
